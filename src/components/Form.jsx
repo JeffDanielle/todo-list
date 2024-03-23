daf7f71f755028f528c8d6d68046292b98ef6c9d
JeffDanielle <bayotjeff@gmail.com>
2024-03-23 22:37:46 +0800

    fixing spacing

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,9 +20,9 @@ const FormAdd = ({ handleAdd }) => {
         setTask('')
     }
     return (
-        <div className="relative lg:left-[41rem] top-15 px-4 py-2 rounded-md text-white">
+        <div className="relative lg:left-[40.5rem] top-15 px-4 py-2 rounded-md text-white">
             <form onSubmit={handleSubmit}>
-                <button className="relative bg-[#606FF5] lg:right-[10rem] px-4 py-2 rounded-md text-white">Add Task</button>
+                <button className="relative bg-[#606FF5] lg:right-[8.5rem] px-4 py-2 rounded-md text-white">Add Task</button>
                 <input type="text" value={task} onChange={(e) => setTask(e.target.value)} className="bg-slate-200 rounded-md w-1/6 indent-2 p-2 text-black" placeholder="Enter a task" />
             </form>
         </div>
